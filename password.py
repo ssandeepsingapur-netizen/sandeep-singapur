84e204b6dee6a600d9d453a7e31b3ff291f6a7fd
sandeep-tech <ssandeepsingapur@gmail.com>
2026-04-17 06:20:01 -0700

    Add files via upload

---
+++ password.py
@@ -0,0 +1,7 @@
+username = (input("Enter your username: "))
+password = (input("Enter your password: "))
+
+if(username == "sandeep" and password == "1234"):
+    print("Login successful!")
+else:
+    print("Invalid username or password")

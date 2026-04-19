4e2866eb265353362a49e29f2aebcbb178ade2be
sandeep-tech <ssandeepsingapur@gmail.com>
2026-04-18 23:00:30 -0700

    Add files via upload

--- python11.py
+++ python11.py
@@ -1,9 +1,29 @@
-import requests
+def add_matrices(mat1, mat2):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return "Matrices must have the same dimension for addiction"
+    result = []
+    for i in range (len(mat1)):
+        row = []
+        for j in range(len(mat1[0])):
+            row.append(mat1[i][j] + mat2[i][j])
+        result.append(row)
+    return result
 
-URL = input("Enter website URl: ")
-response = requests.get(URL)
+matrice1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+matrice2 = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+]
+result = add_matrices(matrice1, matrice2)
 
-if response.status_code == 200:
-    print("Website is up")
+if isinstance(result, str):
+    print(result_matrice)
 else:
-    print("Website is down")
+    print("Result of matrix addiction:")
+    for row in result:
+        print(row)

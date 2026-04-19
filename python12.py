8f3716c63b80021afbf4bdad9ffe12a86f5966f6
sandeep-tech <ssandeepsingapur@gmail.com>
2026-04-18 23:01:16 -0700

    Add files via upload

--- python12.py
+++ python12.py
@@ -1,4 +1,18 @@
-for i in range(1,11):
-    if i == 5:
-        break
-    print(i)
+def transpose_matrix(matrix):
+    row, cols = len(matrix), len(matrix[0])
+    result =  [[0 for _ in range(row)] for _ in range(cols)]
+    
+    for i in range(row):
+        for j in range(cols):
+            result[j][i] = matrix[i][j]
+    
+    return result
+matrix = [
+   [1, 2, 3],
+   [4, 5, 6],
+   [7, 8, 9]
+]
+transpose_matrix = transpose_matrix(matrix)
+ 
+for row in transpose_matrix:
+   print(row)

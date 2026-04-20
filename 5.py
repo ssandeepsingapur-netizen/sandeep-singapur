787453efad441784ab34aba843bbdcc9f8b9a448
sandeep-tech <ssandeepsingapur@gmail.com>
2026-04-20 10:29:36 -0700

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,6 @@
+l = [1, 23, 45, 66,99]
+total = 0
+
+for num in l:
+    total = total + num
+    print(total)
---
+++ 6.py
@@ -0,0 +1,6 @@
+l =[1, 23, 43, 534,32]
+dl = []
+
+for num in l:
+    dl.append(num * 2)
+    print(dl, end=" ")

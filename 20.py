7a1df7f0bbbba417e39ff2eea98e733dfca8a1af
sandeep-tech <ssandeepsingapur@gmail.com>
2026-04-23 09:47:02 -0700

    Add files via upload

---
+++ 20.py
@@ -0,0 +1,7 @@
+def check_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is not a leap year.")
+
+check_year(2026)

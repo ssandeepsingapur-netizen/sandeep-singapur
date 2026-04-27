e54a0326480a94b50828edfda3cd76db5f41fe91
sandeep-tech <ssandeepsingapur@gmail.com>
2026-04-27 10:09:20 -0700

    Add files via upload

---
+++ 30.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+# Creating object
+s1 = Student("Rahul", 20)
+s1.display()
